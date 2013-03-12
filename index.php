e3e12a7393e56348cc5c80c777e780721190e4cb
Clem <clem@clem.(none)>
2013-03-12 13:53:27 +0100

    changed index

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ include_once __DIR__.'/Bittersweet/Bittersweet.php';
 $recipeEngine = new Bittersweet();
 
 // Sample URLs
-$url = 'http://www.marmiton.org/recettes/recette_gateau-a-la-polonaise-de-gabriella_37890.aspx';
+// $url = 'http://www.marmiton.org/recettes/recette_gateau-a-la-polonaise-de-gabriella_37890.aspx';
 // $url = 'http://www.cuisineaz.com/recettes/poulet-au-citron-54773.aspx';
 // $url = 'http://cuisine.journaldesfemmes.com/recette/307392-steak-hache-a-la-japonaise';
-// $url = 'http://www.cuisineaz.com/recettes/truffes-au-chocolat-5675.aspx';
+$url = 'http://www.cuisineaz.com/recettes/truffes-au-chocolat-5675.aspx';
 
 $recette = $recipeEngine->getRecipe($url);
 
